cdf9efdc632c2fddc60c58a683f28ff31e075a54
Camilo arias <camillo47@gmail.com>
2020-12-03 02:56:48 -0500

    Inicio Creación de tablas

--- frontend/covidiejinueve/src/views/Paciente/Index.vue
+++ frontend/covidiejinueve/src/views/Paciente/Index.vue
@@ -1,11 +1,49 @@
 <template>
-    <div>
-        <h1>Pacientes</h1>
-    </div>
+    <div class="overflow-auto">
+    
+
+    <b-button variant="success">Button</b-button>
+    <b-table
+      id="my-table"
+      :items="items"
+      :per-page="perPage"
+      :current-page="currentPage"
+      small
+    ></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+    <p class="mt-3">Pagina actual: {{ currentPage }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name : 'PacientesView'
+    name : 'PacientesView',
+    data() {
+      return {
+        perPage: 3,
+        currentPage: 1,
+        items: [
+          { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+          { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+          { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+          { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+          { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+          { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+          { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+          { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+          { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+        ]
+      }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
+    }
 }
 </script>
